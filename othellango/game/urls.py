bf9ab06301c21c0c072cbc51177b9158648d1c8c
Louis Stevens <louis@louisstevens.com>
2022-03-22 13:45:34 +0000

    Adds basis for disk_moving.

--- othellango/game/urls.py
+++ othellango/game/urls.py
@@ -2,5 +2,6 @@ from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
-	path('', view=views.game_index, name='game_index')
+	path('', view=views.game_index, name='game_index'),
+	path('movedisk', view=views.disk_moved, name='game_index')
 ]
--- othellango/game/views.py
+++ othellango/game/views.py
@@ -5,3 +5,9 @@ from django.shortcuts import render
 
 def game_index(request):
 	return render(request, 'game/game_index.html')
+
+def disk_moved(request):
+	if request.method != 'POST':
+		return HttpResponse(status=405)
+	
+	return HttpResponse("Go away.")
