bab5762a808b4814d5f8784a97b52199e6e4e5f1
Louis Stevens <louis@louisstevens.com>
2022-03-24 14:28:12 +0000

    Adds final backend build.

--- othellango/game/views.py
+++ othellango/game/views.py
@@ -1,7 +1,8 @@
 
+from pprint import pprint
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
-
+import json
 from . import game_handling
 
 # Create your views here.
@@ -13,14 +14,27 @@ def disc_moved(request):
 	if request.method != 'POST':
 		return HttpResponse(status=405)
 	
-	board = request.POST.get("board")
-	lastPlayer = request.POST.get("lastPlayer") # validate these
-	nextPlayer = int(not lastPlayer)
-	print(board, lastPlayer, nextPlayer)
 	
+	jsonBoard = request.POST.get("board")
+	if jsonBoard is None:
+		return HttpResponse(400)
+	
+	try:
+		board = json.loads(request.POST.get("board"))
+	except:
+		return HttpResponse(422)
+	lastPlayer = request.POST.get("lastPlayer") # validate these
+
+	if lastPlayer in [0, 1]:
+		return HttpResponse(422)
+	
+	nextPlayer = int(not lastPlayer)
+	
+
 	board = game_handling.checkBoard(board, lastPlayer)
 	board = game_handling.addPossibleMoves(board, nextPlayer)
 	winner = game_handling.checkForWin(board)
+	
 
 	# Placeholder.
 	responseData = {
